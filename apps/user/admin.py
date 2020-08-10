d1521a7aac6b664aa7f93311e8a7f9fbe671c094
李茂庆 <1060690324@qq.com>
2020-08-10 11:03:09 +0800

    添加用户管理删除模版到静态数据

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Ouser
 # Register your models here.
+
+@admin.register(Ouser)
+class OuserAdmin(admin.ModelAdmin):
+   # date_hierarchy = 'create_date'
+   list_display = ('username',)
