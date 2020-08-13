b17c258b1bbd3b7cc7af19bab1eab9584685cfa9
李茂庆 <1060690324@qq.com>
2020-08-13 15:38:43 +0800

    添加娱乐视图和路由

--- apps/yule/urls.py
+++ apps/yule/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import (index)
+from .views import index,detail
 
 urlpatterns = [
     path('', index, name='index'),
+    path('detail', detail, name='detail'),
 ]
--- apps/yule/views.py
+++ apps/yule/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .sinanew import getSohuYule,getSohuYuleDetail
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the yule index.")
+    # return HttpResponse(getNewsLinkUrl())
+    url = 'https://v2.sohu.com/integration-api/mix/region/131?size=25&adapter=pc&secureScore=50&page={}&callback=jQuery1111'
+    data = getSohuYule(url)
+    return render(request, 'yule/index.html', data)
+
+def detail(request):
+    s =getSohuYuleDetail('https://www.sohu.com/a/412833871_120161664?scm=1002.280027.0.0-0')
+    return render(request, 'yule/detail.html', {'data':s})
